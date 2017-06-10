b8cf120f03c893f27098597f7c98408f5305a8df
xarvh <francesco.orsenigo@gmail.com>
2017-06-10 19:53:28 +1000

    Show texture

--- src/Scene.elm
+++ src/Scene.elm
@@ -2,7 +2,8 @@ module Scene exposing (..)
 
 import Color exposing (Color)
 import Math.Matrix4 as Mat4 exposing (Mat4)
-import Math.Vector3 as Vec3 exposing (vec3, Vec3)
+import Math.Vector2 as Vec2 exposing (Vec2, vec2)
+import Math.Vector3 as Vec3 exposing (Vec3, vec3)
 import WebGL exposing (Mesh, Shader)
 import WebGL.Texture exposing  (Texture)
 
@@ -16,20 +17,20 @@ type alias Uniforms =
     , perspective : Mat4
     , camera : Mat4
     , shade : Float
+    , texture : Texture
     }
 
 
-uniforms : Float -> Uniforms
-uniforms theta =
+uniforms : Float -> Texture -> Uniforms
+uniforms theta texture =
     { rotation =
         Mat4.mul
             (Mat4.makeRotate (3 * theta) (vec3 0 1 0))
             (Mat4.makeRotate (2 * theta) (vec3 1 0 0))
-    , perspective = Mat4.makePerspective 45 1 0.01 100
-    , camera =
-       Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
-         |> Mat4.scale3 (1/viewportWidthHeightRatio) 1 1
+    , perspective = Mat4.makePerspective 45 viewportWidthHeightRatio 0.01 100
+    , camera = Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
     , shade = 1
+    , texture = texture
     }
 
 
@@ -106,33 +107,41 @@ face rawColor a b c d =
 -- Shaders
 
 
-vertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
+vertexShader : Shader Vertex Uniforms { vcolor : Vec3, vcoord : Vec2 }
 vertexShader =
     [glsl|
 
         attribute vec3 position;
         attribute vec3 color;
+
         uniform mat4 perspective;
         uniform mat4 camera;
         uniform mat4 rotation;
+
         varying vec3 vcolor;
+        varying vec2 vcoord;
+
         void main () {
             gl_Position = perspective * camera * rotation * vec4(position, 1.0);
             vcolor = color;
+            vcoord = position.xy;
         }
 
     |]
 
 
-fragmentShader : Shader {} Uniforms { vcolor : Vec3 }
+fragmentShader : Shader {} Uniforms { vcolor : Vec3, vcoord : Vec2 }
 fragmentShader =
     [glsl|
 
         precision mediump float;
         uniform float shade;
+        uniform sampler2D texture;
         varying vec3 vcolor;
+        varying vec2 vcoord;
         void main () {
-            gl_FragColor = shade * vec4(vcolor, 1.0);
+            //gl_FragColor = shade * vec4(vcolor, 1.0);
+            gl_FragColor = texture2D(texture, vcoord);
         }
 
     |]
@@ -148,5 +157,5 @@ entities texture time =
             vertexShader
             fragmentShader
             cubeMesh
-            (uniforms theta)
+            (uniforms theta texture)
         ]
